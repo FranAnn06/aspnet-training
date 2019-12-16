5a06b288b38c2ac2b0995c832640e9ba0e830f72
Нюта Фран <Hanna_f06@mail.ru>
2019-12-16 21:33:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       public static string [] SortStringArray(string[] array)
+        {
+            if (array.Length < 2) 
+            { 
+                return array; 
+            }
+            int p = array[0].Length;
+
+            return SortStringArray(array.Where(x => x.Length < p).ToArray()).Concat(array.Where(x => x.Length == p)).Concat(SortStringArray(array.Where(x => x.Length > p).ToArray())).ToArray();
+        }
+        ////return Array.Empty<string>();
+        ///// }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку через пробел: ");
+            foreach (string s in SortStringArray(Console.ReadLine().Split().ToArray()))
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        }
+    }
+
