83b8f1acf981e556d41195d031c962cac52d0303
Нюта Фран <Hanna_f06@mail.ru>
2019-12-17 13:27:37 +0300

    Добавьте файлы проекта.

---
+++ task01-vowel-count/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace task01_vowel_count
+{
+    class Program
+    {
+        static int CountVowels(string s)
+        {
+            int k = 0;
+            string g = "aeiou";
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < g.Length; j++)
+                {
+                    if (s[i] == g[j])
+                    {
+                        k++;
+                    }
+                }
+            }
+            Console.WriteLine("Number of vowels per line: ");
+            return (k);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string: ");
+            string s = "";
+            s = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(s))
+            {
+                Console.WriteLine(CountVowels(s));
+            }
+            else Console.WriteLine("ArgumentNullException ");
+
+        }
+    }
+}
