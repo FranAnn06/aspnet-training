95112cddbb469ee0ea7b789047fc212ce9f9af9e
Нюта Фран <Hanna_f06@mail.ru>
2019-12-17 11:36:14 +0300

    Добавьте файлы проекта.

---
+++ task03-filter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace task03_filter
+{
+        class Program
+        {
+            static int[] Filter(int[] source)
+            {
+                int[] result = source.Distinct().ToArray();
+                Console.WriteLine("Array without repeating elements: ");
+                Console.WriteLine(string.Join(",", result));
+                return Array.Empty<int>();
+            }
+
+            static void Main()
+            {
+                Console.WriteLine("Enter the size of the array: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] source = new int[n];
+                Random rand = new Random();
+
+                for (int i = 0; i < source.Length; i++)
+                {
+                    source[i] = rand.Next(10);
+                    Console.WriteLine(" " + source[i]);
+                }
+                Filter(source);
+            }
+        }
+    }
+
